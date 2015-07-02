e761ddf63eb7b95c6b4874fa4302c457545344bd
Jonathan Walker <Jonwalker138@gmail.com>
2015-07-02 15:56:37 -0400

    search echoes from server

--- search.js
+++ search.js
@@ -1,8 +1,10 @@
 function main() {
+	var form = $('#searchForm');
+
+	form.submit(submitForm);
 	$('#format').change(toggle3d);
 	$('#advancedLink').click(toggleAdvanced);
 	$('#basicLink').click(toggleAdvanced);
-
 
 	generateYears();
 }
@@ -10,7 +12,7 @@ function main() {
 function toggleAdvanced(event) {
 	//event.preventDefault();
 	console.log('advanced toggled');
-	$('#advancedLink').toggleClass('hidden');
+	$('#basicButtons').toggleClass('hidden');
 	$('#advanced').toggleClass('hidden');
 }
 
@@ -38,6 +40,56 @@ function generateYears() {
 	htmlString += '<option value=\"older\">Before 1900</option>';
 	dropdown.html(htmlString);
 	return;
+}
+
+function submitForm(event) {
+	event.preventDefault();
+
+	var data = new FormData(this),
+	xhr = new XMLHttpRequest(),
+	url = this.action,
+	method='POST';
+	console.log('blah');
+	console.log(this.action);
+	
+// jquery method (broken)
+/*	$.post(url, {contentType: false, processData: false, cache: false})
+	.done(function (data) {
+		$('#outputDiv').html(data);
+	})
+	.fail(function (err) {
+		$('#outputDiv').html(err.status + ': ' + err.statusText);
+	});
+	}*/
+
+
+// xhr method
+/*	xhr.onerror = function (event) {
+		output.html ='Error: ' + xhr.status + ' ' + xhr.statusText;
+	};
+
+	xhr.onreadystatechange = function (event) {
+		var output = $('#outputDiv');
+		if (xhr.DONE === xhr.readyState && 200 === xhr.status) {
+			output.html(xhr.responseText);
+		}
+	};
+
+	xhr.open(method, url);
+	xhr.send(data);
+} */
+
+$.ajax({
+	url: this.action,
+	data: data,
+	cache: false,
+	contentType: false,
+	processData: false,
+	method: 'POST',
+	success: function(data) {
+		$('#outputDiv').html(data);
+	}
+});
 }
 
 $(main);
