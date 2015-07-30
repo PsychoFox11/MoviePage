b24a8d1f51de73abfe164a2d2251abe8cd9fdf58
Jonathan Walker <Jonwalker138@gmail.com>
2015-07-30 14:13:50 -0400

    string and array should live in search

--- mainServer.js
+++ mainServer.js
@@ -44,12 +44,6 @@ function fixQuery(query) { // Corrects datatypes since formData object sends eve
         currentType = dataTypes[key];
 
         switch (currentType) {
-
-            case 'string': {
-                query[key] = new RegExp('^' + query[key] + '$', 'i');
-                break;
-            }
-
             case 'int': {
                 value = parseInt(query[key]);
                 query[key] = isNaN(value) ? null : value;
@@ -59,13 +53,6 @@ function fixQuery(query) { // Corrects datatypes since formData object sends eve
             case 'float': {
                 value = parseFloat(query[key]);
                 query[key] = isNaN(value) ? null : value;
-                break;
-            }
-
-            case 'array': {
-                if (query[key] instanceof Array) {
-                    query[key] = {$in: query[key]};
-                }
                 break;
             }
         }
@@ -136,6 +123,11 @@ app.post('/search', function (req, res) {
 
     for (var key in query) {
         console.log('key: ' + query[key] + ' type: ' + typeof query[key]);
+        if (query[key] instanceof Array) {
+            query[key] = {$in: query[key]};
+        } else if (dataTypes[key] === 'string') {
+            query[key] = new RegExp('^' + query[key] + '$', 'i');
+        }
     }
 
     console.log('query: ' + JSON.stringify(query));
